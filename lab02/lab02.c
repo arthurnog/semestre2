282d7be0eee8f4045b3a94d0381f121cdb6cf2cd
arthurnog <artuts874@gmail.com>
2018-08-27 19:25:03 -0300

    não deu certo upar da ultima vez

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -8,16 +8,24 @@ int main() {
   int col, lin, inten, numeros, i;
   int mask = 0x00000011; /*opaerado para pegar os dois ultimos bits do numero binario*/
   int *p /*vetor*/
-  scanf("%s\n", &tipo);
+  scanf("%c\n", &tipo);
   scanf("%d %d\n", &col, &lin);
   scanf("%d\n", &inten);
   numeros = col*lin;
   p = malloc(numeros * sizeof(int)); /*eu posso fazer isso?*/
   for (i=0; i < numeros; i++)
     scanf("%d", &p[i]);/*alocando os números*/
-    
+
 /*pegar os dois ultimos bits de cada numero e juntar os pares de 4 em 4 formando
 varios numeros binarios, cada um desses numeros representa um caracter que ira
 formar o texto que será printado no fim do programa*/
+
+/*
+  pseudo code:
+  for (i = 0; i< numeros; i+=4)
+    for (j=0; j<4; j++)
+      usar mask(p[i+j]) //isso forma um caractere
+    print caractere //se for \n ele pula a linha se for \0 ele tem que parar
+*/
   return 0;
 }
