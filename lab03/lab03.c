94fa98a712754fa250b58c159a5fedb59ae887ae
arthurnog <artuts874@gmail.com>
2018-09-03 16:01:30 -0300

    deus me ajude

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -15,3 +15,13 @@ int main(){
 
   return 0;
 }
+
+/*
+  pseudo codigo das funcoes
+ criar 4 listas de tamanho indefinido
+ a cada linha lida o primeiro termor vai pra lista CPF, o segundo pra lista NOME
+ o terceiro pra lista EMAIL e o ultimo pra lista IDADE.
+ ao ordenar a lista IDADE a troca de posicao sera feita com as outras listas (talvez
+ isso consuma muita memória, pensar num jeito melhor).
+ printar(CPF[i], NOME[i], EMAIL[i], IDADE[i])
+*/
