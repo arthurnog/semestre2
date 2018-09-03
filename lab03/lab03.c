73c807487c71327b2949ea0a5274dcdb551e6983
arthurnog <artuts874@gmail.com>
2018-09-03 16:21:36 -0300

    começou a ir pra frente

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define int eMax = 30;
+typedef struct /*struct com os dados a serem analisados pelo programa*/
+{
+  long int CPF;
+  char NOME[41];
+  char EMAIL[31];
+  int IDADE;
+} Pessoa;
 
 int main(){
   FILE* arq_in = fopen("read.txt", "r");
   char string[1000];
-
+  //Descobrir o numero de linhas que o texto tem
+  //usar esse numero pra definir o tamanho da lista de pessoas usando malloc
+  //ordenar essa lista usando a idade e o cpf
   while(fscanf(arq_in, "% [^\n]s", string) !=EOF)) {
 
   }
