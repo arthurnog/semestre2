daa1d6d6822c87a5fec67b9e400139c58f40509a
arthurnog <artuts874@gmail.com>
2018-09-03 15:02:27 -0300

    eu fico insistindo no erro, a porra da aula de MC só serve pra me estressar, os filho da puta não cala a boca

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -4,3 +4,13 @@
 #define int nMax = 40
 #define int eMax = 30
 #define int CPF = 11
+
+int main(){
+  FILE* arq_in = fopen("read.txt", "r");
+  char string[1000];
+
+  while(fscanf(arq_in, "% [^\n]s", string) !=EOF);
+    
+
+  return 0;
+}
