49771d6e46b23691bd1fe5d356e9573a87e00d59
arthurnog <artuts874@gmail.com>
2018-09-03 15:23:11 -0300

    não to com cabeça pra fazer lab hoje

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define int nMax = 40
-#define int eMax = 30
-#define int CPF = 11
+#define int eMax = 30;
 
 int main(){
   FILE* arq_in = fopen("read.txt", "r");
   char string[1000];
 
-  while(fscanf(arq_in, "% [^\n]s", string) !=EOF);
-    
+  while(fscanf(arq_in, "% [^\n]s", string) !=EOF)) {
+
+  }
+  fclose(arq_in);
 
   return 0;
 }
