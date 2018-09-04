6ba5dd0aafad74ba9ad11c0cc11e4e8f4b5fc0f5
arthurnog <artuts874@gmail.com>
2018-09-04 17:15:21 -0300

    acho que vai mas ainda nao to confiando

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*tipos são definidos dentro ou fora da main?*/
 typedef struct /*struct com os dados a serem analisados pelo programa*/
 {
   long int CPF;
@@ -10,22 +11,50 @@ typedef struct /*struct com os dados a serem analisados pelo programa*/
   int IDADE;
 } pessoa;
 
+void troca(long int *a, long int *b){
+  int aux = *a
+  *a = *b;
+  *b = aux;
+}
+
+void ordena(pessoa per[], int n){
+  int a, b;
+  for (a=0;a<n-1;a++){
+    for (b=0;b<n-a-1;b++){
+      if (per[j].IDADE > per[j+1].IDADE)
+        troca(&per[j],&per[j+1]);
+      if (per[j].IDADE == per[j+1].IDADE) {
+        if (per[j].CPF > per[j+1].CPF)
+          troca(&per[j],&per[j+1]);
+      }
+    }
+  }
+}
+
 int main(){
-  FILE* arq_in = fopen("read.txt", "r");
+  FILE *arq_in = fopen("read.txt", "r");
   char string[1000];
   int i=0;
-  //Descobrir o numero de linhas que o texto tem
-  //usar esse numero pra definir o tamanho da lista de pessoas usando malloc
+  //usar realloc
   //ordenar essa lista usando a idade e o cpf
   //cpf, nome, email, idade\n
-  //pessoa per*;
-  //per = (pessoa*)realloc(per, n*sizeof(pessoa));
-  while(fscanf(arq_in, "% [^\n]s", string) !=EOF)) {
+  pessoa *per;
+  per = (pessoa*)realloc(per, 10*sizeof(pessoa));
+
+  while(fscanf(arq_in, "%[^\n]s", string) !=EOF)) {
     //enquanto o charactere EOF não aparecer eu vou pegar as informações da linha e aloca na struct
-    /*scanf("%ld, %s, %s, %d\n", per[i].CPF, per[i].NOME,
-                               per[i].EMAIL, per[i].IDADE)*/
-    //i++
+    scanf(arq_in, "%li, %s, %s, %d\n", per[i].CPF, per[i].NOME, per[i].EMAIL, per[i].IDADE);
+    i++; /*i = numero de pessoas*/
   }
+
+  ordena(per,i);
+  //escrever a saida ordenada em outro arquivo (?)
+  FILE *arq_out = fopen("wright.txt", "w");
+  int j;
+  for (j=0; j<i; j++)
+    fprintf (arq_out, "%li, %s, %s, %d\n", per[j].CPF, per[j].NOME, per[j].EMAIL, per[j].IDADE);
+
+  fclose(arq_out);
   fclose(arq_in);
 
   return 0;
