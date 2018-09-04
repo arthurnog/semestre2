73cce1ad5fd63409c8426e28f89e3db20362d978
arthurnog <artuts874@gmail.com>
2018-09-03 22:04:22 -0300

    to tentando mas tá difícil

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -8,16 +8,23 @@ typedef struct /*struct com os dados a serem analisados pelo programa*/
   char NOME[41];
   char EMAIL[31];
   int IDADE;
-} Pessoa;
+} pessoa;
 
 int main(){
   FILE* arq_in = fopen("read.txt", "r");
   char string[1000];
+  int i=0;
   //Descobrir o numero de linhas que o texto tem
   //usar esse numero pra definir o tamanho da lista de pessoas usando malloc
   //ordenar essa lista usando a idade e o cpf
+  //cpf, nome, email, idade\n
+  //pessoa per*;
+  //per = (pessoa*)realloc(per, n*sizeof(pessoa));
   while(fscanf(arq_in, "% [^\n]s", string) !=EOF)) {
-
+    //enquanto o charactere EOF não aparecer eu vou pegar as informações da linha e aloca na struct
+    /*scanf("%ld, %s, %s, %d\n", per[i].CPF, per[i].NOME,
+                               per[i].EMAIL, per[i].IDADE)*/
+    //i++
   }
   fclose(arq_in);
 
