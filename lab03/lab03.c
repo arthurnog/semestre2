171ca86f2d6d562d6bae3e17dad15ab1f25fe49c
arthurnog <artuts874@gmail.com>
2018-09-05 22:40:44 -0300

    realloc(): invalid old size

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -11,8 +11,8 @@ typedef struct /*struct com os dados a serem analisados pelo programa*/
   int IDADE;
 } pessoa;
 
-void troca(long int *a, long int *b){
-  int aux = *a
+void troca(pessoa *a, pessoa *b){
+  pessoa aux = *a;
   *a = *b;
   *b = aux;
 }
@@ -21,11 +21,11 @@ void ordena(pessoa per[], int n){
   int a, b;
   for (a=0;a<n-1;a++){
     for (b=0;b<n-a-1;b++){
-      if (per[j].IDADE > per[j+1].IDADE)
-        troca(&per[j],&per[j+1]);
-      if (per[j].IDADE == per[j+1].IDADE) {
-        if (per[j].CPF > per[j+1].CPF)
-          troca(&per[j],&per[j+1]);
+      if (per[b].IDADE > per[b+1].IDADE)
+        troca(&per[b],&per[b+1]);
+      if (per[b].IDADE == per[b+1].IDADE) {
+        if (per[b].CPF > per[b+1].CPF)
+          troca(&per[b],&per[b+1]);
       }
     }
   }
@@ -41,22 +41,22 @@ int main(){
   pessoa *per;
   per = (pessoa*)realloc(per, 10*sizeof(pessoa));
 
-  while(fscanf(arq_in, "%[^\n]s", string) !=EOF)) {
+  while(fscanf(arq_in, "%[^\n]s", string) !=EOF) {
     //enquanto o charactere EOF não aparecer eu vou pegar as informações da linha e aloca na struct
-    scanf(arq_in, "%li, %s, %s, %d\n", per[i].CPF, per[i].NOME, per[i].EMAIL, per[i].IDADE);
+    fscanf(arq_in, "%li, %s, %s, %d\n", per[i].CPF, per[i].NOME, per[i].EMAIL, per[i].IDADE);
     i++; /*i = numero de pessoas*/
   }
 
   ordena(per,i);
   //escrever a saida ordenada em outro arquivo (?)
-  FILE *arq_out = fopen("wright.txt", "w");
+  FILE *arq_out = fopen("write.txt", "w");
   int j;
   for (j=0; j<i; j++)
     fprintf (arq_out, "%li, %s, %s, %d\n", per[j].CPF, per[j].NOME, per[j].EMAIL, per[j].IDADE);
 
   fclose(arq_out);
   fclose(arq_in);
-
+  free(per);
   return 0;
 }
 
