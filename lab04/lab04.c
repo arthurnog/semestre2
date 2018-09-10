c0c7a53f85827d1fb4ffc01a0de8af7076d24459
arthurnog <artuts874@gmail.com>
2018-09-10 15:01:09 -0300

    quando estiver com mais tempo

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -2,15 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct conj1{
+typedef struct conj1 {
   int elem1;
   struct conj1 *prox;
-};
+}conj1;
 
-struct conj2{
+typedef struct conj2 {
   int elem2;
   struct conj2 *prox;
-};
+}conj2;
+
+/*insere elementos na lista
+void insere(int x, conj *p){
+  conj *nova;
+  nova = malloc (sizeof (celula));
+  nova->elem = x;
+  nova->prox = p->prox;
+  p->prox = nova;
+}
+*/
+
+/*remove elementos da lista
+void remove (conj *p) {
+  conj *trash;
+  trash = p->prox;
+  p->prox = trash->prox;
+  free(trash);
+}
+*/
+
+//https://www.ime.usp.br/~pf/algoritmos/aulas/lista.html
 
 int main(){
 
