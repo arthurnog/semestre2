7b1dbabaed8da0b33cfe13dfaab9ea9260357e27
arthurnog <artuts874@gmail.com>
2018-09-10 14:15:29 -0300

    agora 202 começou de vez

---
+++ lab04/lab04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct conj1{
+  int elem1;
+  struct conj1 *prox;
+};
+
+struct conj2{
+  int elem2;
+  struct conj2 *prox;
+};
+
+int main(){
+
+
+
+  return 0;
+}
