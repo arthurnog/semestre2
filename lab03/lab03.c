7603343ca69b3d6036c171fbb6963ad5f86eaadd
arthurnog <artuts874@gmail.com>
2018-09-13 22:22:35 -0300

    VAI TOMAR NO OLHO DO SEU CUgit add lab03.c! EU ODEIO COMPUTACAO

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -32,25 +32,15 @@ void ordena(pessoa per[], int n){
 }
 
 int main(){
-  FILE *arq_in = fopen("read.txt", "r");
-  char string[1000];
   int i=0;
   int n = 1;
-  //usar realloc
-  //ordenar essa lista usando a idade e o cpf
-  //cpf, nome, email, idade\n
+  /*usar realloc*/
+  /*ordenar essa lista usando a idade e o cpf*/
+  /*cpf, nome, email, idade\n*/
   pessoa *per = NULL;
   per = (pessoa*)realloc(per, n*sizeof(pessoa));
 
-  while(fscanf(arq_in, "%ld, %s %s %d", &per[i].CPF, per[i].NOME, per[i].EMAIL, &per[i].IDADE) != EOF) {
-    //enquanto o charactere EOF não aparecer eu vou pegar as informações da linha e aloca na struct
-
-
-    printf("%ld, %s %s %d\n", per[i].CPF, per[i].NOME, per[i].EMAIL, per[i].IDADE);
-
-    "%d"  4423256,
-    "%s"  ctabtsd,v \n\0
-
+  while(scanf("%ld,%[A-Z A-Z],%s,%d", &per[i].CPF, per[i].NOME, per[i].EMAIL, &per[i].IDADE)) {
     i++; /*i = numero de pessoas*/
     if (i >= n){
       n = n*2;
@@ -60,24 +50,11 @@ int main(){
 
   ordena(per,i);
 
-  //escrever a saida ordenada em outro arquivo (?)
-  FILE *arq_out = fopen("write.txt", "w");
+  /*escrever a saida ordenada*/
   int j;
   for (j=0; j<i; j++)
-    fprintf (arq_out, "%ld, %s %s %d\n", per[j].CPF, per[j].NOME, per[j].EMAIL, per[j].IDADE);
+    printf ("%ld,%s,%s,%d\n", per[j].CPF, per[j].NOME, per[j].EMAIL, per[j].IDADE);
 
-  fclose(arq_out);
-  fclose(arq_in);
   free(per);
   return 0;
 }
-
-/*
-  pseudo codigo das funcoes
- criar 4 listas de tamanho indefinido
- a cada linha lida o primeiro termor vai pra lista CPF, o segundo pra lista NOME
- o terceiro pra lista EMAIL e o ultimo pra lista IDADE.
- ao ordenar a lista IDADE a troca de posicao sera feita com as outras listas (talvez
- isso consuma muita memória, pensar num jeito melhor).
- printar(CPF[i], NOME[i], EMAIL[i], IDADE[i])
-*/
