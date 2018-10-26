a848af6e07ecac0851fac51294ea611d90ad922d
arthurnog <artuts874@gmail.com>
2018-10-26 10:35:33 -0300

    parece que esse vai ser mais complicado que os outros

---
+++ lab07/lab7.c
@@ -0,0 +1,25 @@
+/*Arthur Lucas da Silva Nogueira 213293*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct car{
+  float axl, cont, vel;
+  /*fazer isso usando uma lista ligada ou um vetor mesmo?*/
+}
+int main(){
+  int IT, MC, C, P, i, j;
+  /*IT = numero de iteracoes, MC = numero de carros*/
+  scanf("%d %d", &IT, &MC)
+  for(i=0; i<IT; i++){
+    /*C = numero de carros adicionados,
+      P = numero de carros escolhidos na iteracao*/
+    scanf("%d %d", &C, &P);
+    for(j=0; j<C; j++){
+      /*nesse for os dados dos carros serao guardados*/
+    }
+    for(j=0; j<P; j++){
+      /*nesse for os carros sao escolhidos e retirados do vetor*/
+    }
+  }
+  return 0;
+}
