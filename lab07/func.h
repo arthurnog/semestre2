845fd1115203e1755cca8be678d8e8ea411063dc
arthurnog <artuts874@gmail.com>
2018-10-29 18:16:58 -0300

    ta' complicado

--- lab07/func.h
+++ lab07/func.h
@@ -21,16 +21,6 @@ void troca(car **a, car **b){
   a = b;
   b = c;
 }
-void inserir(heap *fp, car *p, int flag){
-  fp->v[fp->n] = p;
-  fp->n++;
-  if(flag == 0)
-    subir_axl(fp, fp->n-1);
-  else if(flag == 1)
-    subir_cont(fp, fp->n-1);
-  else if(flag == 2)
-    subir_vel(fp, fp->n-1);
-}
 
 void subir_axl(heap *fp, int k){
   if (k>0 && fp->v[pai(k)]->axl <fp->v[k]->axl){
@@ -51,4 +41,15 @@ void subir_vel(heap *fp, int k){
     troca(&fp->v[k], &fp->v[pai(k)]);
     subir_vel(fp, pai(k));
   }
+}
+
+void inserir(heap *fp, car *p, int flag){
+  fp->v[fp->n] = p;
+  fp->n++;
+  if(flag == 0)
+    subir_axl(fp, fp->n-1);
+  else if(flag == 1)
+    subir_cont(fp, fp->n-1);
+  else if(flag == 2)
+    subir_vel(fp, fp->n-1);
 }
