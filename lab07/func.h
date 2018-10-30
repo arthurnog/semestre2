82aebfd84c3d77a5a6fdceca7693729d75c768ff
arthurnog <artuts874@gmail.com>
2018-10-30 16:40:51 -0200

    acho que eu ja terminei as funcoes

--- lab07/func.h
+++ lab07/func.h
@@ -25,6 +25,29 @@ void troca(car **a, car **b){
   b = c;
 }
 
+
+void subir(heap *fp, int k, int flag){
+  if(flag == 0){
+    if (k>0 && fp->v[pai(k)]->axl <fp->v[k]->axl){
+      troca(&fp->v[k], &fp->v[pai(k)]);
+      subir(fp, pai(k), flag);
+    }
+  }
+  else if(flag == 1){
+    if (k>0 && fp->v[pai(k)]->cont <fp->v[k]->cont){
+      troca(&fp->v[k], &fp->v[pai(k)]);
+      subir(fp, pai(k), flag);
+    }
+  }
+  else if(flag == 2){
+    if (k>0 && fp->v[pai(k)]->vel <fp->v[k]->vel){
+      troca(&fp->v[k], &fp->v[pai(k)]);
+      subir(fp, pai(k), flag);
+    }
+  }
+}
+
+/*
 void subir_axl(heap *fp, int k){
   if (k>0 && fp->v[pai(k)]->axl <fp->v[k]->axl){
     troca(&fp->v[k], &fp->v[pai(k)]);
@@ -45,16 +68,48 @@ void subir_vel(heap *fp, int k){
     subir_vel(fp, pai(k));
   }
 }
-
+*/
 void inserir(heap *fp, car *p, int flag){
   fp->v[fp->n] = p;
   fp->n++;
+  subir(fp, fp->n-1, flag);/*
   if(flag == 0)
     subir_axl(fp, fp->n-1);
   else if(flag == 1)
     subir_cont(fp, fp->n-1);
   else if(flag == 2)
-    subir_vel(fp, fp->n-1);
+    subir_vel(fp, fp->n-1);*/
+}
+
+void descer(heap *fp, int k, int flag){
+  int maior_filho;
+  if(F_ESQ(k) < fp->n){
+    maior_filho = F_ESQ (k);
+    if(flag == 0){
+      if (F_DIR(k) < fp->n && fp->v[F_ESQ(k)]->axl < fp->v[F_DIR(k)]->axl)
+        maior_filho = F_DIR(k);
+      if(fp->v[k]->axl < fp->v[maior_filho]->axl){
+        troca(&fp->v[k], &fp->v[maior_filho]);
+        descer(fp, maior_filho, flag);
+      }
+    }
+    else if(flag == 1){
+      if (F_DIR(k) < fp->n && fp->v[F_ESQ(k)]->cont < fp->v[F_DIR(k)]->cont)
+        maior_filho = F_DIR(k);
+      if(fp->v[k]->cont < fp->v[maior_filho]->cont){
+        troca(&fp->v[k], &fp->v[maior_filho]);
+        descer(fp, maior_filho, flag);
+      }
+    }
+    else if(flag == 2){
+      if (F_DIR(k) < fp->n && fp->v[F_ESQ(k)]->vel < fp->v[F_DIR(k)]->vel)
+        maior_filho = F_DIR(k);
+      if(fp->v[k]->vel < fp->v[maior_filho]->vel){
+        troca(&fp->v[k], &fp->v[maior_filho]);
+        descer(fp, maior_filho, flag);
+      }
+    }
+  }
 }
 
 void extrair(heap *fp, int flag){
@@ -62,11 +117,6 @@ void extrair(heap *fp, int flag){
   printf("%f %f %f\n", fp->v[0]->axl, fp->v[0]->cont, fp->v[0]->vel);
   troca(&fp->v[0], &fp->v[fp->n -1]);
   fp->n--;
-  if(flag == 0)
-    descer_axl(fp, 0);
-  else if(flag == 1)
-    descer_cont(fp, 0);
-  else if(flag == 2)
-    descer_vel(fp, 0);
+  descer(fp, 0, flag);
   free(fp->v[n]);
 }
