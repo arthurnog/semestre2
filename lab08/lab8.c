ae2836d52cb698d9ca9d0e375b55d32de3b68235
arthurnog <artuts874@gmail.com>
2018-11-05 10:27:20 -0200

    nao consegui o lab 7 e o lab 8 e pra hoje sem falta

---
+++ lab08/lab8.c
@@ -0,0 +1,19 @@
+/*Arthur Lucas da Silva Nogueira 213293*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  int N = 9; char ordm[4] = "123"; int a;
+  while(N != 0 && ordm != "0"){
+    scanf("%d %s\n", &N, ordm);
+    int i;
+    for(i = 0; i<N; i++){
+      scanf("%d", &a);
+      /*inserir a na arvore*/
+    }
+    /*diametro(arvore);*/
+    /*imprimir(arvore, ordm);*/
+  }
+  return 0;
+}
