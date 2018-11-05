d6fee33bd1a30403e9bcbccad1fa281f78fadd80
arthurnog <artuts874@gmail.com>
2018-11-05 15:10:49 -0200

    acho que acabou

--- lab08/lab8.c
+++ lab08/lab8.c
@@ -8,17 +8,25 @@ typedef struct no {
   struct no *esq, *dir;
 } no;
 
+no *busca(no *r, int v);
+no *inserir(no *r, int v);
+no *liberar(no *r);
+int altura(no *r);
+void diametro(no *r);
+void imprimir(no *r, char cmd[]);
+
 int main(){
   int N = 9; char ordm[4] = "123"; int a;
   while(N != 0 && ordm != "0"){
     scanf("%d %s\n", &N, ordm);
-    int i;
+    int i; no *tree = NULL;
     for(i = 0; i<N; i++){
       scanf("%d", &a);
-      /*arvore = inserir a na arvore*/
+      tree = inserir(tree, a);
     }
-    /*diametro(arvore);*/
-    /*imprimir(arvore, ordm);*/
+    diametro(tree);
+    imprimir(tree, ordm);
+    tree = liberar(tree);
   }
   return 0;
 }
@@ -49,8 +57,50 @@ no *inserir(no *r, int v){
   return r;
 }
 
-/*
-Pré-ordem: Você deve visitar primeiro a raiz, depois a sub-árvore esquerda e por último a sub-árvore direita.
-Em-ordem: Você deve visitar primeiro a sub-árvore esquerda, depois a raiz e por último a sub-árvore direita.
-Pós-ordem: Você deve visitar primeiro a sub-árvore esquerda, depois a sub-árvore direita e por último a raiz.
-*/
+no *liberar(no *r){
+  if(r != NULL){
+    r->esq = liberar(r->esq);
+    r->dir = liberar(r->dir);
+    free(r);
+  }
+  return NULL;
+}
+
+int altura(no *r){
+  int h_esq, h_dir;
+  if(r == NULL)
+    return 0;
+  else{
+    h_esq = 1+altura(r->esq);
+    h_dir = 1+altura(r->dir);
+    if(h_esq > h_dir)
+      return h_esq;
+    else
+      return h_dir;
+  }
+}
+
+void diametro(no *r){
+  int D = 1+altura(r->esq)+altura(r->dir);
+  printf("Diametro da arvore binaria: %d", D)
+}
+
+void imprimir(no *r, char cmd[]){
+  if(r != NULL){
+    if(cmd == "pre"){
+      printf("%d", r->info);
+      imprimir(r->esq, cmd);
+      imprimir(r->dir, cmd);
+    }
+    else if(cmd == "in"){
+      imprimir(r->esq, cmd);
+      printf("%d", r->info);
+      imprimir(r->dir, cmd);
+    }
+    else if(cmd == "pos"){
+      imprimir(r->esq, cmd);
+      imprimir(r->dir, cmd);
+      printf("%d", r->info);
+    }
+  }
+}
