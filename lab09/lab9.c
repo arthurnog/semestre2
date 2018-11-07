f92809099f51f341ca1db7951a42e5c58d6a9929
arthurnog <artuts874@gmail.com>
2018-11-07 10:46:19 -0200

    tenho que comecar de algum lugar nao e mesmo

--- lab09/lab9.c
+++ lab09/lab9.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+//8
 int main(){
-
+  char word[9];
+  /*como as palavras reservadas sao todas em letras minusculas e nenhuma delas
+  tem mais de 8 letras e valido fazer isso*/
+  while(scanf("%[a-z]", word) != EOF){
+    /*if word esta na lista de palavras reservadas*/
+      /*inserir word na arvore AVL*/
+  }
+  /*impressao zig zag*/
   return 0;
 }
