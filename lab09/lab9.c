8e39acfe4dfe400ab1bd87eb821864bd27e23a27
arthurnog <artuts874@gmail.com>
2018-11-09 16:25:18 -0200

    nao acho que eu vou conseguir entregar esse lab

--- lab09/lab9.c
+++ lab09/lab9.c
@@ -127,12 +127,69 @@ no *rot_zig(no *x){
   return y;
 }
 
+no *minimo(no *r){
+  if(strcmp(r->dir->palavra, r->esq->palavra)<0)
+    return r->dir;
+  else
+    return r->esq;
+}
+
+no *remover(no *r, char word[]){
+  no *temp;
+  int balanceamento;
+  if(r==NULL)
+    return r;
+  if(strcmp(word, r->palavra)<0)
+    r->esq = remover(r->esq, word);
+  else if(strcmp(word,r->palavra)>0)
+    r->dir = remover(r->dir, word);
+  else{
+    if((r->esq == NULL) || (r->dir == NULL)){
+      temp = r->esq ? r->esq : r->dir;
+      if(temp == NULL){
+        temp = r;
+        r = NULL;
+      }
+      else
+        *r = *temp;
+      free(temp);
+    }
+    else{
+      temp = minimo(r->dir);
+      strcpy(r->palavra, temp->palavra);
+      r->dir = remover(r->dir, temp->palavra);
+    }
+  }
+  if(r == NULL)
+    return r;
+  r->altura = 1 + max(altura(r->esq), altura(r->dir));
+  balanceamento = fator_balanceamento(r);
+
+  if(balanceamento > 1 && fator_balanceamento(r->esq) >= 0)
+    return rot_dir(r);
+
+  if(balanceamento > 1 && fator_balanceamento(r->esq) < 0){
+    r->esq = rot_esq(r->esq);
+    return rot_dir(r);
+  }
+
+  if(balanceamento < -1 && fator_balanceamento(r->dir) <=0)
+    return rot_esq(r);
+
+  if(balanceamento < -1 && fator_balanceamento(r->dir) > 0){
+    r->dir = rot_dir(r->dir);
+    return rot_esq(r);
+  }
+
+  return r;
+}
+
 void imprimir_arvore(no *raiz){
   /*a impressao deve ser feita em zig-zag. Eu poderia tambem remover a chave
     apos a impressao da palavra que esta nela*/
   if(raiz == NULL)
     return;
   else{
-    
+
   }
 }
